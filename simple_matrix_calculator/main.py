cdbeb413745199d6b929a387a1de9d246be716c1
supermistral <mediamart37@gmail.com>
2022-10-18 01:42:37 +0300

    completed task 3 - simple matrix calculator

---
+++ simple_matrix_calculator/main.py
@@ -0,0 +1,75 @@
+from matrix import Matrix
+from typing import Union
+
+
+def print_matrix(matrix: Matrix) -> None:
+    matrix_list = matrix.get_matrix()
+    for i in range(matrix.size()[0]):
+        print(*matrix_list[i])
+
+
+def get_operand(input_str: str) -> Union[Matrix, float]:
+    try:
+        return float(input_str)
+    except:
+        size = tuple(map(int, input_str.split()))
+
+    matrix_list = []
+    for _ in range(size[0]):
+        matrix_list.append(list(map(float, input().split())))
+
+    return Matrix(matrix_list)
+
+
+def main():
+    print("SIMPLE MATRIX CALCULATOR\n\n"
+          "You need to write data in format:\n<operand>\n<operation>\n<operand>\n"
+          "Operand must include size in format '<rows> <columns>' (i.e '3 2')\n"
+          "You can type a number instead\n"
+          "Supported operations: + | - | * | ^-1 | det | transpose\n"
+          "If inverse matrix operation (^-1) is entered, 2nd operand is not required\n"
+          "To quit type /q\n")
+
+    while True:
+        input_str_1 = input("Number or matrix size > ").strip()
+
+        if input_str_1 == '/q':
+            break
+
+        operand_1 = get_operand(input_str_1)
+        operation = input("Operation > ").strip()
+
+        if operation in ['^-1', 'det', 'transpose']:
+            try:
+                if operation == '^-1':
+                    print_matrix(operand_1.inverse())
+                elif operation == 'det':
+                    print(operand_1.determinant())
+                elif operation == 'transpose':
+                    print_matrix(operand_1.transpose())
+            except Exception as e:
+                print("[ERROR]:", e)
+            
+            print()
+            continue
+
+        operand_2 = get_operand(input("Number or matrix size > ").strip())
+        print()
+
+        try:
+            if operation == '+':
+                print_matrix(operand_1 + operand_2)
+            elif operation == '-':
+                print_matrix(operand_1 - operand_2)
+            elif operation == '*':
+                print_matrix(operand_1 * operand_2)
+            else:
+                print('Unknown operation')
+        except Exception as e:
+            print("[ERROR]:", e)
+
+        print()
+
+
+if __name__ == '__main__':
+    main()
---
+++ simple_matrix_calculator/tests.py
@@ -0,0 +1,177 @@
+import pytest
+
+from matrix import Matrix
+
+
+@pytest.fixture
+def matrix_a_2x2():
+    return Matrix([[1, 2], [3, 4]])
+
+
+@pytest.fixture
+def matrix_b_2x2():
+    return Matrix([[2, 4], [6, 8]])
+
+
+@pytest.fixture
+def matrix_a_3x3():
+    return Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+
+@pytest.fixture
+def matrix_b_3x2():
+    return Matrix([[0, 2], [4, 6], [8, 0]])
+
+
+def test_size(matrix_a_2x2: Matrix, matrix_b_2x2: Matrix,
+              matrix_a_3x3: Matrix, matrix_b_3x2: Matrix):
+    assert matrix_a_2x2.size() == (2, 2)
+    assert matrix_b_2x2.size() == (2, 2)
+    assert matrix_a_3x3.size() == (3, 3)
+    assert matrix_b_3x2.size() == (3, 2)
+
+
+def test_minor(matrix_a_2x2: Matrix, matrix_a_3x3: Matrix, matrix_b_3x2: Matrix):
+    assert matrix_a_2x2.minor(1, 1) == 4
+    assert matrix_a_2x2.minor(1, 2) == 3
+    assert matrix_a_2x2.minor(2, 1) == 2
+    assert matrix_a_2x2.minor(2, 2) == 1
+
+    assert matrix_a_3x3.minor(1, 1) == -3
+    assert matrix_a_3x3.minor(2, 2) == -12
+    assert matrix_a_3x3.minor(3, 3) == -3
+
+    with pytest.raises(ValueError):
+        matrix_b_3x2.minor(1, 1)
+
+
+def test_determinant(matrix_a_2x2: Matrix, matrix_b_2x2: Matrix,
+                     matrix_a_3x3: Matrix, matrix_b_3x2: Matrix):
+    assert matrix_a_2x2.determinant() == -2
+    assert matrix_b_2x2.determinant() == -8
+    assert matrix_a_3x3.determinant() == 0
+    
+    with pytest.raises(ValueError):
+        matrix_b_3x2.determinant()
+
+
+def test_cofactor(matrix_a_2x2: Matrix, matrix_a_3x3: Matrix, matrix_b_3x2: Matrix):
+    assert matrix_a_2x2.cofactor(1, 1) == 4
+    assert matrix_a_2x2.cofactor(1, 2) == -3
+    assert matrix_a_2x2.cofactor(2, 1) == -2
+    assert matrix_a_2x2.cofactor(2, 2) == 1
+
+    assert matrix_a_3x3.cofactor(1, 1) == -3
+    assert matrix_a_3x3.cofactor(1, 2) == 6
+    assert matrix_a_3x3.cofactor(2, 2) == -12
+    assert matrix_a_3x3.cofactor(2, 3) == 6
+    assert matrix_a_3x3.cofactor(3, 3) == -3
+
+    with pytest.raises(ValueError):
+        matrix_b_3x2.cofactor(1, 1)
+
+
+def test_is_square(matrix_a_2x2: Matrix, matrix_b_2x2: Matrix,
+                   matrix_a_3x3: Matrix, matrix_b_3x2: Matrix):
+    assert matrix_a_2x2.is_square()
+    assert matrix_b_2x2.is_square()
+    assert matrix_a_3x3.is_square()
+    assert not matrix_b_3x2.is_square()
+
+
+def test_get_matrix(matrix_a_2x2: Matrix, matrix_b_2x2: Matrix,
+                    matrix_a_3x3: Matrix, matrix_b_3x2: Matrix):
+    assert matrix_a_2x2.get_matrix() == [[1, 2], [3, 4]]
+    assert matrix_b_2x2.get_matrix() == [[2, 4], [6, 8]]
+    assert matrix_a_3x3.get_matrix() == [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    assert matrix_b_3x2.get_matrix() == [[0, 2], [4, 6], [8, 0]]
+
+
+def test_multiplication_by_number(matrix_a_2x2: Matrix, matrix_b_2x2: Matrix,
+                                  matrix_a_3x3: Matrix, matrix_b_3x2: Matrix):
+    new_matrix_a_2x2 = matrix_a_2x2 * 2
+    new_matrix_b_2x2 = matrix_b_2x2 * 2
+    new_matrix_a_3x3 = matrix_a_3x3 * 5
+    new_matrix_b_3x2 = matrix_b_3x2 * 10
+
+    assert isinstance(new_matrix_a_2x2, Matrix)
+    assert isinstance(new_matrix_b_2x2, Matrix)
+    assert isinstance(new_matrix_a_3x3, Matrix)
+    assert isinstance(new_matrix_b_3x2, Matrix)
+
+    assert new_matrix_a_2x2.size() == (2, 2)
+    assert new_matrix_b_2x2.size() == (2, 2)
+    assert new_matrix_a_3x3.size() == (3, 3)
+    assert new_matrix_b_3x2.size() == (3, 2) 
+    assert new_matrix_a_2x2.get_matrix() == [[2, 4], [6, 8]]
+    assert new_matrix_b_2x2.get_matrix() == [[4, 8], [12, 16]]
+    assert new_matrix_a_3x3.get_matrix() == [[5, 10, 15], [20, 25, 30], [35, 40, 45]]
+    assert new_matrix_b_3x2.get_matrix() == [[0, 20], [40, 60], [80, 0]]
+
+
+def test_multiplication_by_matrix(matrix_a_2x2: Matrix, matrix_b_2x2: Matrix,
+                                  matrix_a_3x3: Matrix, matrix_b_3x2: Matrix):
+    new_matrix_a_2x2 = matrix_a_2x2 * matrix_b_2x2
+    new_matrix_a_3x2 = matrix_a_3x3 * matrix_b_3x2
+
+    assert isinstance(new_matrix_a_2x2, Matrix)
+    assert isinstance(new_matrix_a_3x2, Matrix)
+
+    assert new_matrix_a_2x2.size() == (2, 2)
+    assert new_matrix_a_3x2.size() == (3, 2) 
+    assert new_matrix_a_2x2.get_matrix() == [[14, 20], [30, 44]]
+    assert new_matrix_a_3x2.get_matrix() == [[32, 14], [68, 38], [104, 62]]
+
+    with pytest.raises(ArithmeticError):
+        matrix_a_2x2 * matrix_a_3x3
+
+
+def test_addition(matrix_a_2x2: Matrix, matrix_b_2x2: Matrix,
+                  matrix_a_3x3: Matrix, matrix_b_3x2: Matrix):
+    new_matrix_a_2x2 = matrix_a_2x2 + matrix_b_2x2
+    new_matrix_a_3x3 = matrix_a_3x3 + matrix_a_3x3
+
+    assert isinstance(new_matrix_a_2x2, Matrix)
+    assert isinstance(new_matrix_a_3x3, Matrix)
+
+    assert new_matrix_a_2x2.size() == (2, 2)
+    assert new_matrix_a_3x3.size() == (3, 3) 
+    assert new_matrix_a_2x2.get_matrix() == [[3, 6], [9, 12]]
+    assert new_matrix_a_3x3.get_matrix() == [[2, 4, 6], [8, 10, 12], [14, 16, 18]]
+
+    with pytest.raises(ArithmeticError):
+        matrix_a_2x2 + matrix_b_3x2
+
+def test_subtraction(matrix_a_2x2: Matrix, matrix_b_2x2: Matrix,
+                     matrix_a_3x3: Matrix, matrix_b_3x2: Matrix):
+    new_matrix_a_2x2 = matrix_a_2x2 - matrix_b_2x2
+    new_matrix_a_3x3 = matrix_a_3x3 - matrix_a_3x3
+
+    assert isinstance(new_matrix_a_2x2, Matrix)
+    assert isinstance(new_matrix_a_3x3, Matrix)
+
+    assert new_matrix_a_2x2.size() == (2, 2)
+    assert new_matrix_a_3x3.size() == (3, 3) 
+    assert new_matrix_a_2x2.get_matrix() == [[-1, -2], [-3, -4]]
+    assert new_matrix_a_3x3.get_matrix() == [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+    with pytest.raises(ArithmeticError):
+        matrix_a_2x2 - matrix_b_3x2
+
+
+def test_transposition(matrix_a_2x2: Matrix, matrix_b_2x2: Matrix,
+                       matrix_a_3x3: Matrix, matrix_b_3x2: Matrix):
+    assert matrix_a_2x2.transpose().get_matrix() == [[1, 3], [2, 4]]
+    assert matrix_b_2x2.transpose().get_matrix() == [[2, 6], [4, 8]]
+    assert matrix_a_3x3.transpose().get_matrix() == [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+    assert matrix_b_3x2.transpose().get_matrix() == [[0, 4, 8], [2, 6, 0]]
+
+
+def test_inverse_matrix(matrix_a_2x2: Matrix, matrix_b_2x2: Matrix,
+                        matrix_a_3x3: Matrix, matrix_b_3x2: Matrix):
+    assert matrix_a_2x2.inverse().get_matrix() == [[-2, 1], [1.5, -0.5]]
+    assert matrix_b_2x2.inverse().get_matrix() == [[-1, 0.5], [0.75, -0.25]]
+
+    with pytest.raises(ArithmeticError):
+        matrix_a_3x3.inverse()
+        matrix_b_3x2.inverse()
